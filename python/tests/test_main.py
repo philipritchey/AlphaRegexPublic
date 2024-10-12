8f20ac706955fb117dfec11d4c5cf25e826a93fe
philip ritchey <pcr@tamu.edu>
2024-10-12 16:21:59 -0500

    autocommit: passing all tests with >= 90% coverage

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -5,4 +5,4 @@ def test_main():
   main('../benchmarks/no02_end_with_01')
   # main('../benchmarks/no03_substring_0101')
   main('../benchmarks/no04_begin_1_end_0')
-  # main('../benchmarks/no05_length_at_least3_and_third_0')
+  main('../benchmarks/no05_length_at_least3_and_third_0')
