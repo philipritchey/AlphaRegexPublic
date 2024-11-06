e82651c86226864b9f554d5f293f3bce6bb29897
philip ritchey <pcr@tamu.edu>
2024-11-06 16:55:34 -0600

    autocommit: passing all tests with >= 90% coverage

--- python/main/search.py
+++ python/main/search.py
@@ -3,7 +3,7 @@ Search
 '''
 
 import heapq
-from main.partial_regex import PartialRegexNode, Hole, opt
+from main.partial_regex import PartialRegexNode, Hole, opt, Star, Union, Literal, Concatenation
 from main.helpers import inflate_all
 
 def search(P: set[str], N: set[str], alphabet: str = '01') -> str:
@@ -25,11 +25,15 @@ def search(P: set[str], N: set[str], alphabet: str = '01') -> str:
   v_pre: set[PartialRegexNode] = set()
   v_post: set[PartialRegexNode] = set()
   cost_limit = 252
+  target_state = Star(Union(Literal('0'), Concatenation(Literal('1'), Hole())))
   while True:
     state = heapq.heappop(q)
-    # print(f'[DEBUG] {state=} {state}')
+    if state == target_state:
+      print()
+      print(f'[DEBUG] {state=} {state} {state.cost()}')
     if state in v_post:
-      # print('        skipped')
+      if state == target_state:
+        print('        skipped')
       continue
     v_post.add(state)
     if state.is_solution(P, N):
@@ -37,12 +41,18 @@ def search(P: set[str], N: set[str], alphabet: str = '01') -> str:
     if not state.is_dead(P, N):
       # expand and add to queue
       for next_state in state.next_states(alphabet):
-        # print(f'        {next_state=} {next_state}', end='')
-        if next_state.cost() <= cost_limit and next_state not in v_pre:
-          heapq.heappush(q, next_state)
-          v_pre.add(next_state)
+        if state == target_state:
+          print(f'        {next_state=} {next_state}', end='')
+        if next_state.cost() <= cost_limit:
+          if next_state not in v_pre:
+            heapq.heappush(q, next_state)
+            v_pre.add(next_state)
     #       print(' added')
     #     else:
     #       print(' NOT added')
-    # else:
-    #   print('        dead')
+        else:
+          if state == target_state:
+            print(f"{next_state} is too expensive: {next_state.cost()}")
+    else:
+      if state == target_state:
+        print('        dead')
